5bc2cfcf77d8e11244d78b9bc34119f59f32de5b
Mitchell <mwong51@cs.washington.edu>
2020-02-10 15:46:35 -0800

    first commit

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+import alpaca_trade_api as tradeapi
+API_KEY = "our key here"
+API_SECRET_KEY = "our secret key here"
+api = tradeapi.REST(API_KEY, API_SECRET_KEY, api_version='v2')
+
+class EquityScreener:
+    def __init__(self, stocks, priceEarnings, dividend):
+        #self.alpaca = 
+        self.stockPool = stocks
+        self.priceEarningsRatio = priceEarnings
+        self.dividendYield = dividend
+    
 
