ae967912e4c1e751af46890e1e8fab0bf01ba095
Mitchell <mwong51@cs.washington.edu>
2020-02-10 16:11:38 -0800

    "getFinancials method"

--- Main.py
+++ Main.py
@@ -6,8 +6,20 @@ api = tradeapi.REST(API_KEY, API_SECRET_KEY, api_version='v2')
 class EquityScreener:
     def __init__(self, stocks, priceEarnings, dividend):
         #self.alpaca = 
-        self.stockPool = stocks
+        self.stockPool = ["MSFT", "AAPL", "AMZN"]
         self.priceEarningsRatio = priceEarnings
         self.dividendYield = dividend
-    
+        self.finalStockPool = []
+
+    def getFinancials(self):
+        dictionaryEarnings = api.polygon.financials(self.stockPool)
+        for stock in self.stockPool:
+            stockFinancials = dictionaryEarnings[stock]
+            stockResults = stockFinancials["results"]
+            stockFinancialInfo = stockResults[0]
+            stockPtoE = stockFinancialInfo["priceToEarningsRatio"]
+            stockDividend = stockFinancialInfo["dividendYield"]
+            if self.priceEarningsRatio < stockPtoE and self.dividendYield > stockDividend:
+                self.finalStockPool.append(stock)
+            
 
